e6a6aa05da960d52939c57fc97c795d54277de11
Helios Figueroa <hfigueroa@abargon.com>
2019-02-24 15:03:14 -0600

    ajuste API

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -146,10 +146,12 @@
                         <div class="col-lg-6 mb-4 mb-lg-0">
                           <div class="card rounded credit-card bg-hover-gradient-green">
                             <div class="content d-flex flex-column justify-content-between p-4">
-                              <h1 class="mb-5"><i class="fab fa-cc-discover"></i></h1>
+                              <h1 class="mb-5">
+                                <img src="{{ asset('img/logos/compartamos.png') }}" alt="" style="width: 30%;">
+                              </h1>
                               <div class="d-flex justify-content-between align-items-end pt-3">
                                 <div class="text-uppercase">
-                                  <div class="font-weight-bold d-block">Card Number</div><small class="text-gray">1245 1478 1362 6985</small>
+                                  <div class="font-weight-bold d-block">Número de tarjeta</div><small class="text-gray">0102 1390 11</small>
                                 </div>
                                 <h4 class="mb-0">$568.00</h4>
                               </div>
